30c6c10834020c00b957d039f2faabd8a01238ff
sand14 <sand.avrigeanu@yahoo.com>
2024-05-14 02:56:43 +0300

    Added register file and docker compose

--- angular/src/app/pages/login/login.component.ts
+++ angular/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {AuthenticationRequest} from "../../services/models/authentication-request";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../../services/services/authentication.service";
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,15 @@ export class LoginComponent {
   errorMsg: Array<string> = [];
 
   constructor(private router: Router,
-              private authService: AuthenticationService) {
+              private authService: AuthenticationService,
+              private tokenService: TokenService) {
   }
   login() {
     this.errorMsg = [];
     this.authService.authenticate({
       body: this.authRequest,
     }).subscribe({next: (res) =>{
-      // todo save the token
+      this.tokenService.token = res.token as string;
       this.router.navigate(['calendar'])
       },
     error: (err) =>{
